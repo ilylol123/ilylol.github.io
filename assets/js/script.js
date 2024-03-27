9bd4a3ee3ebe0ca06c65719e0dfc2e883fe86cc2
ilylol123 <90770502+ilylol123@users.noreply.github.com>
2024-03-27 19:14:44 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,8 @@ function switchScreen() {
     });
   });
 
-  ['background', 'rain'].forEach(function (audioName) {
+  //['background', 'rain'].forEach(function (audioName) {
+    ['background'].forEach(function (audioName) {
     let fullPath = `assets/audio/${audioName}.mp3`;
 
     let audioElement = document.createElement('audio');
